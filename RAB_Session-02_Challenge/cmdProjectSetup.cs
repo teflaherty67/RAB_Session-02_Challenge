61d4d8bdc0d87d6bac6004f164222e2cf72c68c9
Tom E Flaherty <teflaherty@utexas.edu>
2022-12-13 22:41:56 -0600

    solution complete

--- RAB_Session-02_Challenge/cmdProjectSetup.cs
+++ RAB_Session-02_Challenge/cmdProjectSetup.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 
 #endregion
 
@@ -27,21 +28,76 @@ namespace RAB_Session_02_Challenge
 
             // declare variables
 
-            string filePathLevels = @"S:\Personal Folders\Training Material\ArchSmarter\Revit Add-in Bootcamp\Session 02\Session 02_Room List.csv";
-            string fileText = System.IO.File.ReadAllText(filePath);
+            string filePathLevels = @"S:\Personal Folders\Training Material\ArchSmarter\Revit Add-in Bootcamp\Session 02\RAB_Session_02_Challenge_Levels.csv";
+            string filePathSheets = @"S:\Personal Folders\Training Material\ArchSmarter\Revit Add-in Bootcamp\Session 02\RAB_Session_02_Challenge_Sheets.csv";
+
+            List<string[]> levelData = new List<string[]>();
+            List<string[]> sheetData = new List<string[]>();
 
             // read text file data
 
+            string[] arrayLevels = File.ReadAllLines(filePathLevels);
+            string[] arraySheets = File.ReadAllLines(filePathSheets);
+
             // loop through file data and put into lists
+
+            foreach(string levelString in arrayLevels)
+            {
+                string[] cellData = levelString.Split(',');
+
+                levelData.Add(cellData);
+            }
+
+            foreach (string sheetString in arraySheets)
+            {
+                string[] cellData = sheetString.Split(',');
+
+                sheetData.Add(cellData);
+            }
 
             // remove header rows
 
+            levelData.RemoveAt(0);
+            sheetData.RemoveAt(0);
+
             // create levels
+
+            Transaction t1 = new Transaction(doc);
+            t1.Start("Create Levels");
+
+            foreach (string[] curLevelData in levelData)
+            {
+                string stringHeight = curLevelData[1];
+                double levelHeight = 0;
+                bool convertFeet = double.TryParse(stringHeight, out levelHeight);
+
+                Level curLevel = Level.Create(doc, levelHeight);
+                curLevel.Name = curLevelData[0];
+            }
+
+            t1.Commit();
+            t1.Dispose();
 
             // get titleblock element ID
 
+            FilteredElementCollector colTBlocks = new FilteredElementCollector(doc);
+            colTBlocks.OfCategory(BuiltInCategory.OST_TitleBlocks);
+            ElementId tblockID = colTBlocks.FirstElementId();
+
             // create sheets
 
+            Transaction t2 = new Transaction(doc);
+            t2.Start("Create Sheets");
+
+            foreach (string[] curSheetData in sheetData)
+            {
+                ViewSheet curSheet = ViewSheet.Create(doc, tblockID);
+                curSheet.SheetNumber = curSheetData[0];
+                curSheet.Name = curSheetData[1];
+            }
+
+            t2.Commit();
+            t2.Dispose();
 
             return Result.Succeeded;
         }
